2fdb35f93c4282e41f927042342a569be4079691
Sushma-2004 <130671668+Sushma-2004@users.noreply.github.com>
2024-10-04 01:25:42 +0530

    head and body of main

---
+++ lib/utils/AppColors/app_colors.dart
@@ -0,0 +1,13 @@
+import 'dart:ui';
+
+class AppColors{
+  static final Color textColor = const Color(0xFFccc7c5);
+  static final Color mainColor = const Color(0xFFF06292);
+  static final mainBlackColor = const Color(0xFF332d2b);
+  static final iconColor1 = const Color(0xFFffd28d);
+  static final iconColor2 = const Color(0xFFfcab88);
+  static final paraColor = const Color(0xFF8f837f);
+  static final buttonBackgroundColor = const Color(0xFFf7ff6f4);
+  static final signColor = const Color(0xFFa9a29f);
+}
+
---
+++ lib/widgets/big_text.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class BigText extends StatelessWidget {
+  Color? color;
+  final String text;
+  double size;
+  TextOverflow overFlow;
+  BigText({super.key, this.color = const Color(0xFF332d2b) , 
+        required this.text,
+        this.overFlow = TextOverflow.ellipsis,
+        this.size=20});
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+       text,
+       maxLines: 1,
+       overflow: overFlow,
+       style: TextStyle(
+        color:color,
+        fontFamily: 'Roboto',
+        fontWeight: FontWeight.w400,
+        fontSize: size
+       ),
+    );
+  }
+}
